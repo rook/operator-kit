016ae0462e29ff1a1664dd24395a2eab09867e9c
Travis Nielsen <travisn@live.com>
2017-06-28 17:19:12 -0700

    working operator sample

--- sample-operator/README.md
+++ sample-operator/README.md
@@ -25,16 +25,39 @@ kubectl create -f sample-operator.yaml
 kubectl create -f sample-resource.yaml
 ```
 
+### Modify the Sample Resource
+Change the value of the `Hello` property in the operator, then apply the new yaml.
+```bash
+kubectl apply -f sample-resource.yaml
+```
+
 ### Artifacts
 Now look at the artifacts of the operator and resources created.
 ```bash
 $ kubectl get pod -l app=sample-operator
+NAME                              READY     STATUS    RESTARTS   AGE
+sample-operator-821691060-m5vqp   1/1       Running   0          3m
 
 $ kubectl get thirdpartyresource sample.mycompany.io
+NAME                  DESCRIPTION                             VERSION(S)
+sample.mycompany.io   A very simple example custom resource   v1alpha1
 
 $ kubectl get sample.mycompany.io
+NAME       KIND
+mysample   Sample.v1alpha1.mycompany.io
 
+# Notice the added and modified Hello= text in the log below
 $ kubectl logs -l app=sample-operator
+2017-06-29 00:11:22.738629 I | sample: Creating the sample resource
+2017-06-29 00:11:22.749325 I | op-kit: creating sample resource
+2017-06-29 00:11:22.794635 I | op-kit: did not yet find resource sample at apis/mycompany.io/v1alpha1/samples. the server could not find the requested resource
+2017-06-29 00:11:28.797620 I | op-kit: did not yet find resource sample at apis/mycompany.io/v1alpha1/samples. the server could not find the requested resource
+2017-06-29 00:11:34.797912 I | sample: Managing the sample resource
+2017-06-29 00:11:34.797932 I | sample: finding existing samples...
+2017-06-29 00:11:34.799276 I | sample: found 0 samples.
+2017-06-29 00:11:34.799296 I | op-kit: start watching sample resource in namespace default at 27064
+2017-06-29 00:12:07.605948 I | sample: Added Sample 'mysample' with Hello=World!
+2017-06-29 00:14:29.553035 I | sample: Modified Sample 'mysample' with Hello=ANOTHER World!!
 ```
 
 ### Cleanup
--- sample-operator/operator.go
+++ sample-operator/operator.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/coreos/pkg/capnslog"
 	opkit "github.com/rook/operator-kit"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 const resourceGroup = "mycompany.io"
@@ -13,24 +15,49 @@ const resourceGroup = "mycompany.io"
 var logger = capnslog.NewPackageLogger("github.com/rook/operator-kit", "sample")
 
 func main() {
-	context := opkit.KubeContext{}
-
-	// initialize the sample resource
-	httpCli, err := opkit.NewHTTPClient(resourceGroup)
+	logger.Infof("Getting kubernetes context")
+	context, err := createContext()
 	if err != nil {
-		fmt.Printf("failed to create http client. %+v\n", err)
+		logger.Errorf("failed to create context. %+v\n", err)
 		os.Exit(1)
 	}
-	context.KubeHTTPCli = httpCli.Client
 
-	err = opkit.CreateCustomResources(context, []opkit.CustomResource{sampleResource})
+	logger.Infof("Creating the sample resource")
+	err = opkit.CreateCustomResources(*context, []opkit.CustomResource{sampleResource})
 	if err != nil {
-		fmt.Printf("failed to create custom resource. %+v\n", err)
+		logger.Errorf("failed to create custom resource. %+v\n", err)
 		os.Exit(1)
 	}
 
 	// start watching the sample resource
-	logger.Infof("Managing the sample custom resources")
-	mgr := &sampleManager{namespace: "default", context: context}
+	logger.Infof("Managing the sample resource")
+	mgr := &sampleManager{namespace: "default", context: *context}
 	mgr.Manage()
+}
+
+func createContext() (*opkit.KubeContext, error) {
+	// create the k8s client
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get k8s config. %+v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get k8s client. %+v", err)
+	}
+
+	// initialize the sample resource
+	httpCli, err := opkit.NewHTTPClient(resourceGroup)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create http client. %+v", err)
+	}
+
+	return &opkit.KubeContext{
+		MasterHost:  config.Host,
+		Clientset:   clientset,
+		KubeHTTPCli: httpCli.Client,
+		RetryDelay:  6,
+		MaxRetries:  15,
+	}, nil
 }
